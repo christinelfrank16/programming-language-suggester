a599569c5f5bf992e1f2cb1ea622f90be50089f8
Christine Frank <christine.braun13@gmail.com>
2019-08-16 10:54:48 -0700

    Add js logic to show additional questions

--- js/scripts.js
+++ js/scripts.js
@@ -1,4 +1,13 @@
 $(document).ready(function(){
+  var data = {};
+
+  $("form#quiz-intro").submit(function(event){
+    event.preventDefault();
+    $("#second-quiz").children().hide();
+    showSecondQuiz(data);
+  });
+
+
   // get and show initial slider values
   updateSliders();
   // update slider values when moved
@@ -11,10 +20,34 @@ $(document).ready(function(){
 
 
 
-function updateSliders(){
+function updateSliders(data){
   var experience = $("#prior-exp").val();
   $("#exp-value").text(experience);
 
   var effort = $("#effort").val();
   $("#effort-value").text(effort);
+}
+
+
+function showSecondQuiz(data){
+  // Get and store variables to data object
+  var learn = parseInt($("input:radio[name=learn]:checked").val());
+  var experience = parseInt($("#prior-exp").val());
+  var styling = parseInt($("#style-type").val());
+  var effort = parseInt($("#effort").val());
+
+  data.experience = experience;
+  data.effort = effort;
+  data.learn = learn;
+  data.styling = styling;
+
+  // determine which 2nd quiz and show
+  if((experience > 3 && effort >= 6) || experience >= 6) {
+    $("#group-2a").show();
+    console.log("A")
+  }
+  else {
+    $("#group-2b").show();
+    console.log("B")
+  }
 }
